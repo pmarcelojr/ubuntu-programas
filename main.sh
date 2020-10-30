174303d90ae6439f326c0aa53d66a21ac61512b8
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-10-30 16:18:34 -0300

    included install chrome e visual studio code

--- main.sh
+++ main.sh
@@ -12,3 +12,13 @@ echo
 echo "*-* INSTALL GIT *-*"
 #sudo apt install git
 echo
+echo "*-* CONFIG MOSTRA % BATERIA *-*"
+#gsettings set org.gnome.desktop.interface show-battery-percentage true
+echo
+echo "*-* INSTALL CHROME *-*"
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo apt install ./google-chrome-stable_current_amd64.deb
+#rm -rf google-chrome-stable_current_amd64
+echo
+echo "*-* INSTALL VISUAL STUDIO CODE *-*"
+#sudo snap install --classic code
