57c4296a705bf6399c447e40085cf7a1c993e15c
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-10-30 17:29:51 -0300

    included install teams/ zoom

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "*-* Verificando o Sistema por Atualizações... *-*"
-if ! sudo apt update
-then
-    echo "Não foi possivel atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list"
-    exit 1
-fi
+#if ! sudo apt update
+#then
+#    echo "Não foi possivel atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list"
+#    exit 1
+#fi
 echo
 echo "*-* Mostrando as Atualizações do Sistema... *-*"
 #sudo apt list --upgradable
@@ -29,3 +29,37 @@ echo "*-* INSTALL CHROME *-*"
 echo
 echo "*-* INSTALL VISUAL STUDIO CODE *-*"
 #sudo snap install --classic code
+echo
+echo `Escolha uma plataforma de comunicação de video`
+echo "[ 1 ] TEAMS"
+echo "[ 2 ] ZOOM"
+echo "[ 3 ] TEAMS & ZOOM"
+echo "[ 4 ] Sair"
+echo
+echo -n "Qual a opção desejada? "
+read OPCAO
+case $OPCAO in
+    1) echo "Instalando TEAMS"
+        #wget "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=linux&arch=x64&download=true&linuxArchiveType=deb" -O teams.deb
+        #sudo apt install -y ./teams.deb
+        #rm -rf teams.deb
+        ;;
+    2) echo "Instalando ZOOM"
+        #wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
+        #sudo apt install -y ./zoom.deb 
+        #rm -rf zoom.deb
+        ;;
+    3) echo "Instalando TEAMS e ZOOM"
+        echo "TEAMS"
+        #wget "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=linux&arch=x64&download=true&linuxArchiveType=deb" -O teams.deb
+        #sudo apt install -y ./teams.deb
+        #rm -rf teams.deb
+        echo "ZOOM"
+        #wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
+        #sudo apt install -y ./zoom.deb 
+        #rm -rf zoom.deb
+        ;;
+    4) echo "Saindo..."
+        exit ;;
+    *) "Opção desconhecida." ;
+esac
