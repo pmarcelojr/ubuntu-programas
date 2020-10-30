6ee228834d4cab7a89a95521f33bad572664b72d
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-10-30 16:04:08 -0300

    script inicial update/upgrade/git

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "*-* UPDATE *-*"
+#sudo apt update
+clear
+echo "*-* LIST --UPGRADABLE *-*"
+#sudo apt list --upgradable
+echo
+echo "*-* UPGRADE *-*"
+#sudo apt upgrade -y
+echo
+echo "*-* INSTALL GIT *-*"
+#sudo apt install git
+echo
